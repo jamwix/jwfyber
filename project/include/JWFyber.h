4b09da345f2e1501cfacd3c9ccf7b9adf01380cd
Bill Booth <wrbooth@gmail.com>
2015-09-04 14:03:57 -0700

    fixing extern C symbol issue

--- project/include/JWFyber.h
+++ project/include/JWFyber.h
@@ -4,13 +4,16 @@
 
 namespace jwfyber {
 	
-    void jwfStart(const char *sAppId, const char *sUserId, const char *sToken, bool useToast);
-    void jwfLaunchOfferWall();
-    void jwfPauseDownloads();
-    void jwfResumeDownloads();
-    void jwfRequestOffer();
-    void jwfRequestOfferWithReward(const char *sRewardType, const char *sPlacement);
-    void jwfShowVideoAd();
+    extern "C"
+    {
+        void jwfStart(const char *sAppId, const char *sUserId, const char *sToken, bool useToast);
+        void jwfLaunchOfferWall();
+        void jwfPauseDownloads();
+        void jwfResumeDownloads();
+        void jwfRequestOffer();
+        void jwfRequestOfferWithReward(const char *sRewardType, const char *sPlacement);
+        void jwfShowVideoAd();
+    }
 	
 }
 
